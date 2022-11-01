ad09913990a428e6b112efae38f2ddf53f4a5ced
gurvir-kaur <geer97.gkk@gmail.com>
2022-11-01 11:18:48 +0100

    Libreria Datatables añadido +  creación de la páginas (Productos, añadirProductos).

--- Controller/ProductoController.cs
+++ Controller/ProductoController.cs
@@ -11,8 +11,8 @@ namespace pruebaMediaMarkt.Controllers
             return View();
         }
 
-        // GET: ProductoController/Details/5
-        public ActionResult Details(int id)
+        // GET: ProductoController/Productos
+        public ActionResult Productos()
         {
             return View();
         }
@@ -38,46 +38,5 @@ namespace pruebaMediaMarkt.Controllers
             }
         }
 
-        // GET: ProductoController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductoController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ProductoController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductoController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
---
+++ Pages/Prodcutos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pruebaMediaMarkt.Pages
+{
+    public class ProdcutosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
